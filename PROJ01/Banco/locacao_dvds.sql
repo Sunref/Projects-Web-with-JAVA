c6d4307053ffe63805ea0ed4fc2a178977847ac9
Fernanda <143466223+Sunref@users.noreply.github.com>
2025-10-06 13:11:16 -0300

    UPDATE: adjusting database for mariadb;

--- PROJ01/Banco/locacao_dvds.sql
+++ PROJ01/Banco/locacao_dvds.sql
@@ -1,55 +1,42 @@
--- MySQL Workbench Forward Engineering
-
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
 -- -----------------------------------------------------
 -- Schema locacao_dvds
 -- -----------------------------------------------------
+CREATE DATABASE IF NOT EXISTS locacao_dvds CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE locacao_dvds;
 
 -- -----------------------------------------------------
--- Schema locacao_dvds
+-- Table genero
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS locacao_dvds DEFAULT CHARACTER SET utf8 ;
-USE locacao_dvds ;
-
--- -----------------------------------------------------
--- Table locacao_dvds.genero
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS locacao_dvds.genero (
+CREATE TABLE IF NOT EXISTS genero (
   id INT NOT NULL AUTO_INCREMENT,
   descricao VARCHAR(40) NOT NULL,
-  PRIMARY KEY (id))
-ENGINE = InnoDB;
-
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
 -- -----------------------------------------------------
--- Table locacao_dvds.classificacao_etaria
+-- Table classificacao_etaria
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS locacao_dvds.classificacao_etaria (
+CREATE TABLE IF NOT EXISTS classificacao_etaria (
   id INT NOT NULL AUTO_INCREMENT,
   descricao VARCHAR(40) NOT NULL,
-  PRIMARY KEY (id))
-ENGINE = InnoDB;
-
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
 -- -----------------------------------------------------
--- Table locacao_dvds.ator
+-- Table ator
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS locacao_dvds.ator (
+CREATE TABLE IF NOT EXISTS ator (
   id INT NOT NULL AUTO_INCREMENT,
   nome VARCHAR(45) NOT NULL,
   sobrenome VARCHAR(45) NOT NULL,
   data_estreia DATE NOT NULL,
-  PRIMARY KEY (id))
-ENGINE = InnoDB;
-
+  PRIMARY KEY (id)
+) ENGINE=InnoDB;
 
 -- -----------------------------------------------------
--- Table locacao_dvds.dvd
+-- Table dvd
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS locacao_dvds.dvd (
+CREATE TABLE IF NOT EXISTS dvd (
   id INT NOT NULL AUTO_INCREMENT,
   titulo VARCHAR(100) NOT NULL,
   ano_lancamento INT NOT NULL,
@@ -60,33 +47,25 @@ CREATE TABLE IF NOT EXISTS locacao_dvds.dvd (
   classificacao_etaria_id INT NOT NULL,
   genero_id INT NOT NULL,
   PRIMARY KEY (id),
-  INDEX fk_dvd_ator_principal_idx (ator_principal_id ASC),
-  INDEX fk_dvd_ator_coadjuvante_idx (ator_coadjuvante_id ASC),
-  INDEX fk_dvd_classificacao_etaria_idx (classificacao_etaria_id ASC),
-  INDEX fk_dvd_genero_idx (genero_id ASC),
+  INDEX fk_dvd_ator_principal_idx (ator_principal_id),
+  INDEX fk_dvd_ator_coadjuvante_idx (ator_coadjuvante_id),
+  INDEX fk_dvd_classificacao_etaria_idx (classificacao_etaria_id),
+  INDEX fk_dvd_genero_idx (genero_id),
   CONSTRAINT fk_dvd_ator_principal
-    FOREIGN KEY (ator_principal_id)
-    REFERENCES locacao_dvds.ator (id)
+    FOREIGN KEY (ator_principal_id) REFERENCES ator (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_dvd_ator_coadjuvante
-    FOREIGN KEY (ator_coadjuvante_id)
-    REFERENCES locacao_dvds.ator (id)
+    FOREIGN KEY (ator_coadjuvante_id) REFERENCES ator (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_dvd_classificacao_etaria
-    FOREIGN KEY (classificacao_etaria_id)
-    REFERENCES locacao_dvds.classificacao_etaria (id)
+    FOREIGN KEY (classificacao_etaria_id) REFERENCES classificacao_etaria (id)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT fk_dvd_genero
-    FOREIGN KEY (genero_id)
-    REFERENCES locacao_dvds.genero (id)
+    FOREIGN KEY (genero_id) REFERENCES genero (id)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+    ON UPDATE NO ACTION
+) ENGINE=InnoDB;
 
-
-SET SQL_MODE=@OLD_SQL_MODE;
-SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
-SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
